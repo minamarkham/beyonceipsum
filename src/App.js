a6e21d6725a1bd5be8b736691ee77e9ebada5f6f
Mina Markham <mina.markham@gmail.com>
2023-10-24 16:22:40 -0700

    clean up padding

--- src/App.js
+++ src/App.js
@@ -133,7 +133,6 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		document.title = 'Beyoncé Ipsum';
 		// Generate Lorem Ipsum text on component load
 		dispatch(
 			generateLoremIpsum({
@@ -182,129 +181,124 @@ const App = () => {
 			}}
 		>
 			<div className="app">
-				<div className="container">
-					<header className="header">
-						<p className="header__title">Beyoncé Ipsum</p>
-					</header>
-					<main className="main">
-						<h1 className="main__title">{title}</h1>
-						<Form
-							className="form"
-							layout="vertical"
-							autoComplete="off"
-							initialValues={{
-								paragraphLength: 'medium',
-							}}
-						>
-							<Form.Item name="slider" label="# of Paragraphs">
-								<div className="slider__label">
-									<Slider
-										min={1}
-										max={6}
-										step={1}
-										defaultValue={3}
-										tooltip={{ open: true }}
-										onChange={(e) => setParagraphs(e)}
-									/>
-								</div>
-							</Form.Item>
-							<Form.Item
-								name="paragraphLength"
-								label="Paragraph Length"
-							>
-								<Radio.Group
-									onChange={(e) =>
-										setParagraphLength(e.target.value)
-									}
-									value={paragraphLength}
-									buttonStyle="solid"
-								>
-									<Radio.Button value="short">
-										Short
-									</Radio.Button>
-									<Radio.Button value="medium">
-										Medium
-									</Radio.Button>
-									<Radio.Button value="long">
-										Long
-									</Radio.Button>
-								</Radio.Group>
-							</Form.Item>
-							<Form.Item label="Which Albums?">
-								<Checkbox.Group
-									options={albumOptions}
-									value={selectedAlbums}
-									onChange={handleAlbumSelection}
+				<header className="header">
+					<p className="header__title">Beyoncé Ipsum</p>
+				</header>
+				<main className="main">
+					<h1 className="main__title">{title}</h1>
+					<Form
+						className="form"
+						layout="vertical"
+						autoComplete="off"
+						initialValues={{
+							paragraphLength: 'medium',
+						}}
+					>
+						<Form.Item name="slider" label="# of Paragraphs">
+							<div className="slider__label">
+								<Slider
+									min={1}
+									max={6}
+									step={1}
+									defaultValue={3}
+									tooltip={{ open: true }}
+									onChange={(e) => setParagraphs(e)}
 								/>
-								<Checkbox
-									indeterminate={indeterminate}
-									onChange={onCheckAllChange}
-									checked={checkAll}
-								>
-									Check all
-								</Checkbox>
-							</Form.Item>
-							<Form.Item>
-								<Checkbox
-									checked={includeSlay}
-									onChange={(e) =>
-										setIncludeSlay(e.target.checked)
-									}
-								>
-									Slay 💃🏾💃🏾💃🏾
-								</Checkbox>
-								<br />
-								<Checkbox
-									checked={includeExplicit}
-									onChange={(e) =>
-										setIncludeExplicit(e.target.checked)
-									}
-								>
-									Include Explicit Lyrics
-								</Checkbox>
-								<br />
-								<Checkbox
-									checked={includeGreeting}
-									onChange={(e) =>
-										setIncludeGreeting(e.target.checked)
-									}
-								>
-									Start with "Hey, Ms. Carter!"
-								</Checkbox>
-							</Form.Item>
-							<HappyProvider>
-								<Button
-									type="primary"
-									ghost
-									onClick={handleGenerateIpsum}
-								>
-									Generate
-								</Button>
-							</HappyProvider>
-						</Form>
-						<section className="generated">
-							<div
-								dangerouslySetInnerHTML={{
-									__html: loremIpsumText,
-								}}
+							</div>
+						</Form.Item>
+						<Form.Item
+							name="paragraphLength"
+							label="Paragraph Length"
+						>
+							<Radio.Group
+								onChange={(e) =>
+									setParagraphLength(e.target.value)
+								}
+								value={paragraphLength}
+								buttonStyle="solid"
+							>
+								<Radio.Button value="short">Short</Radio.Button>
+								<Radio.Button value="medium">
+									Medium
+								</Radio.Button>
+								<Radio.Button value="long">Long</Radio.Button>
+							</Radio.Group>
+						</Form.Item>
+						<Form.Item label="Which Albums?">
+							<Checkbox.Group
+								options={albumOptions}
+								value={selectedAlbums}
+								onChange={handleAlbumSelection}
 							/>
-							<div className="copy">
-								<Dropdown
-									menu={{ items, onClick: onDropdownClick }}
-								>
-									<Button type="primary" ghost>
-										Copy
-										<svg
-											fill="white"
-											version="1.1"
-											xmlns="http://www.w3.org/2000/svg"
-											x="0px"
-											y="0px"
-											viewBox="0 0 368.008 368.008"
-											width={16}
-										>
-											<path
-												d="M368,88.004c0-1.032-0.224-2.04-0.6-2.976c-0.152-0.376-0.416-0.664-0.624-1.016c-0.272-0.456-0.472-0.952-0.832-1.352
+							<Checkbox
+								indeterminate={indeterminate}
+								onChange={onCheckAllChange}
+								checked={checkAll}
+							>
+								Check all
+							</Checkbox>
+						</Form.Item>
+						<Form.Item>
+							<Checkbox
+								checked={includeSlay}
+								onChange={(e) =>
+									setIncludeSlay(e.target.checked)
+								}
+							>
+								Slay 💃🏾💃🏾💃🏾
+							</Checkbox>
+							<br />
+							<Checkbox
+								checked={includeExplicit}
+								onChange={(e) =>
+									setIncludeExplicit(e.target.checked)
+								}
+							>
+								Include Explicit Lyrics
+							</Checkbox>
+							<br />
+							<Checkbox
+								checked={includeGreeting}
+								onChange={(e) =>
+									setIncludeGreeting(e.target.checked)
+								}
+							>
+								Start with "Hey, Ms. Carter!"
+							</Checkbox>
+						</Form.Item>
+						<HappyProvider>
+							<Button
+								type="primary"
+								ghost
+								onClick={handleGenerateIpsum}
+							>
+								Generate
+							</Button>
+						</HappyProvider>
+					</Form>
+					<section className="generated">
+						<div
+							dangerouslySetInnerHTML={{
+								__html: loremIpsumText,
+							}}
+						/>
+						<div className="copy">
+							<Dropdown
+								menu={{ items, onClick: onDropdownClick }}
+							>
+								<Button type="primary" ghost>
+									Copy
+									<svg
+										fill="white"
+										version="1.1"
+										xmlns="http://www.w3.org/2000/svg"
+										x="0px"
+										y="0px"
+										viewBox="0 0 368.008 368.008"
+										width={16}
+									>
+										<path
+											d="M368,88.004c0-1.032-0.224-2.04-0.6-2.976c-0.152-0.376-0.416-0.664-0.624-1.016c-0.272-0.456-0.472-0.952-0.832-1.352
 										l-72.008-80c-1.512-1.688-3.672-2.656-5.944-2.656h-15.648c-0.232,0-0.472,0-0.704,0H151.992c-13.232,0-24,10.768-24,24v40H24
 										c-13.232,0-24,10.768-24,24v256c0,13.232,10.768,24,24,24h192c13.232,0,24-10.768,24-24v-40h104c13.232,0,24-10.768,24-24v-175.96
 										c0-0.016,0.008-0.024,0.008-0.04L368,88.004z M224,344.004c0,4.408-3.592,8-8,8H24c-4.408,0-8-3.592-8-8v-256c0-4.408,3.592-8,8-8
@@ -313,33 +307,34 @@ const App = () => {
 										c0-0.016-0.024-0.016-0.016-0.016c0,0-0.008-0.008-0.008-0.016c-0.008,0-0.016-0.008-0.016-0.016
 										c-0.032-0.032-0.072-0.072-0.112-0.112l-80-80c-1.504-1.504-3.544-2.352-5.664-2.352h-8.008v-40c0-4.408,3.592-8,8-8h112v88
 										c0,4.416,3.584,8,8,8H352V280.004z M352,96.004h-72.008v-80h4.44L352,91.076V96.004z"
-											></path>
-										</svg>
-									</Button>
-								</Dropdown>
-								{copied && (
-									<span className="copied">
-										Copied to clipboard!
-									</span>
-								)}
-							</div>
-						</section>
-					</main>
-				</div>
-				<div className="container">
-					<footer className="footer">
-						<p className="footer__note">
-							Made by{' '}
-							<a
-								className="footer__link"
-								href="https://mina.codes"
-							>
-								Mina
-							</a>{' '}
-							• Not affliated with Parkwood Entertainment
-						</p>
-					</footer>
-				</div>
+										></path>
+									</svg>
+								</Button>
+							</Dropdown>
+							{copied && (
+								<span className="copied">
+									Copied to clipboard!
+								</span>
+							)}
+						</div>
+					</section>
+				</main>
+				<footer className="footer">
+					<p className="footer__note">
+						Made by{' '}
+						<a className="footer__link" href="https://mina.codes">
+							Mina
+						</a>{' '}
+						•{' '}
+						<a
+							className="footer__link"
+							href="https://github.com/minamarkham/beyonceipsum"
+						>
+							Contribute
+						</a>{' '}
+						• Not affliated with Parkwood Entertainment
+					</p>
+				</footer>
 			</div>
 		</ConfigProvider>
 	);
